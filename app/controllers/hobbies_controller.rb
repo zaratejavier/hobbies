11e08f78b46fb2bda1629ab3947e333fa37cd126
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-22 14:52:55 -0600

    added sign up, login, and controller

---
+++ app/controllers/hobbies_controller.rb
@@ -0,0 +1,49 @@
+class HobbiesController < ApplicationController
+  before_action :set_hobbies, :only [:show, :edit, :update]
+  
+  
+  
+  def index
+    @hobby = current_user.hobbies
+  end
+
+  def show
+  end
+
+  def new
+    @hobby = Hobby.new
+  end
+
+  def create
+    @hobby = current_user.hobbies.new(hobby_params)
+    if @hobby.save 
+      redirect to hobby_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @hobby.update(hobby_params)
+      redirect_to hobbies_path
+    else
+      render :edit
+    end
+  end
+  
+  def edit
+  end
+  
+  private
+  def hobby_params
+    parmas.require(:hobby).permit(:name, :description)
+  end
+
+def set_hobbies 
+  @hobby = current.user.hobbies.find(params[:id])
+end
+end
+
+
+
+
